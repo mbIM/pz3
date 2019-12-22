ddff5c56e4ebbe5fe516c75eba570f311a105b6e
mbIM <lina.alina.fedotova@gmail.com>
2019-12-22 20:43:43 +0300

    l

---
+++ num1/num1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace num1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
